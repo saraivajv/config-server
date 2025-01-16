2b8c406d860ec76dda1c1cdd90b5cf02e6d4b61c
saraivajv <joaovictors2010@gmail.com>
2025-01-16 19:03:07 -0300

    Upando spring config server

---
+++ config-server/src/test/java/com/imd/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imd.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
